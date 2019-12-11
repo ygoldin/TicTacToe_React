ff42e32a00447ce0a456b2751aba213af459163a
Yael Goldin <ygoldin@cs.washington.edu>
2019-12-10 16:34:43 -0800

    improved tests

--- test/TestSpot.cpp
+++ test/TestSpot.cpp
@@ -3,10 +3,29 @@
 #include <gtest/gtest.h>
 
 namespace TicTacToe {
+    
+    class SpotTest : public ::testing::Test {
+        protected:
+          Spot m_spot;
+    };
 
-TEST(Spot, CreateEmptySpot) {
-    Spot s;
-    ASSERT_EQ(s.getStatus(), SpotStatus::Empty);
+TEST_F(SpotTest, CreatedEmptySpot) {
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::Empty);
+}
+
+TEST_F(SpotTest, SetEmptySpot) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::Empty));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::Empty);
+}
+
+TEST_F(SpotTest, SetX) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::X));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::X);
+}
+
+TEST_F(SpotTest, SetO) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::O));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::O);
 }
 
 } // namespace TicTacToe
