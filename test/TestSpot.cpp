6924d04516313a61b0d1aff5f7bba9a31e3b25df
Yael Goldin <ygoldin@cs.washington.edu>
2019-12-10 16:38:18 -0800

    finished spot tests

--- test/TestSpot.cpp
+++ test/TestSpot.cpp
@@ -13,7 +13,7 @@ TEST_F(SpotTest, CreatedEmptySpot) {
     ASSERT_EQ(m_spot.getStatus(), SpotStatus::Empty);
 }
 
-TEST_F(SpotTest, SetEmptySpot) {
+TEST_F(SpotTest, SetEmpty) {
     ASSERT_TRUE(m_spot.updateStatus(SpotStatus::Empty));
     ASSERT_EQ(m_spot.getStatus(), SpotStatus::Empty);
 }
@@ -25,6 +25,42 @@ TEST_F(SpotTest, SetX) {
 
 TEST_F(SpotTest, SetO) {
     ASSERT_TRUE(m_spot.updateStatus(SpotStatus::O));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::O);
+}
+
+TEST_F(SpotTest, SetEmptyWhileX) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::X));
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::Empty));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::Empty);
+}
+
+TEST_F(SpotTest, SetXWhileX) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::X));
+    ASSERT_FALSE(m_spot.updateStatus(SpotStatus::X));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::X);
+}
+
+TEST_F(SpotTest, SetOWhileX) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::X));
+    ASSERT_FALSE(m_spot.updateStatus(SpotStatus::O));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::X);
+}
+
+TEST_F(SpotTest, SetEmptyWhileO) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::O));
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::Empty));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::Empty);
+}
+
+TEST_F(SpotTest, SetXWhileO) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::O));
+    ASSERT_FALSE(m_spot.updateStatus(SpotStatus::X));
+    ASSERT_EQ(m_spot.getStatus(), SpotStatus::O);
+}
+
+TEST_F(SpotTest, SetOWhileO) {
+    ASSERT_TRUE(m_spot.updateStatus(SpotStatus::O));
+    ASSERT_FALSE(m_spot.updateStatus(SpotStatus::O));
     ASSERT_EQ(m_spot.getStatus(), SpotStatus::O);
 }
 
